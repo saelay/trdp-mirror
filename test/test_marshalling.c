eedb537618647640cab84248d33ac667a4054660
97031 <97031@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2012-10-25 16:09:28 +0000

    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk/trdp@79 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- test/test_marshalling.c
+++ test/test_marshalling.c
@@ -11,12 +11,80 @@
 #include <string.h>
 #include "tau_marshall.h"
 
-/*	Test data set	*/
-TRDP_DATASET_T gDataSet1000 =
+/*	Test data sets	*/
+TRDP_DATASET_T gDataSet990 =
 {
-    1000,       /*	dataset/com ID  */
+    990,       /*	dataset/com ID  */
     0,          /*	reserved		*/
-    32,         /*	No of elements	*/
+    1,         /*	No of elements, var size	*/
+    {           /*	TRDP_DATASET_ELEMENT_T[]	*/
+        {
+            TRDP_UINT8,
+            1
+        },
+        {
+            TRDP_CHAR8,
+            16
+        }
+    }
+};
+
+TRDP_DATASET_T gDataSet991 =
+{
+    991,       /*	dataset/com ID  */
+    0,          /*	reserved		*/
+    1,         /*	No of elements, var size	*/
+    {           /*	TRDP_DATASET_ELEMENT_T[]	*/
+        {
+            TRDP_UINT8,
+            1
+        },
+        {
+            990,
+            1
+        }
+    }
+};
+
+TRDP_DATASET_T gDataSet992 =
+{
+    992,       /*	dataset/com ID  */
+    0,          /*	reserved		*/
+    1,         /*	No of elements, var size	*/
+    {           /*	TRDP_DATASET_ELEMENT_T[]	*/
+        {
+            TRDP_UINT8,
+            1
+        },
+        {
+            991,
+            1
+        }
+    }
+};
+
+TRDP_DATASET_T gDataSet993 =
+{
+    993,       /*	dataset/com ID  */
+    0,          /*	reserved		*/
+    1,         /*	No of elements, var size	*/
+    {           /*	TRDP_DATASET_ELEMENT_T[]	*/
+        {
+            TRDP_UINT8,
+            1
+        },
+        {
+            992,
+            1
+        }
+    }
+};
+
+TRDP_DATASET_T gDataSet999 =
+{
+    999,        /*	dataset/com ID  */
+    0,          /*	reserved		*/
+    65,         /*	No of elements	*/
     {           /*	TRDP_DATASET_ELEMENT_T[]	*/
         {
             TRDP_BOOLEAN8,  /*	data type		*/
@@ -92,7 +160,7 @@ TRDP_DATASET_T gDataSet1000 =
         },
        {
             TRDP_UTF16,
-            4
+            16
         },
         {
             TRDP_INT8,
@@ -145,8 +213,164 @@ TRDP_DATASET_T gDataSet1000 =
         {
             TRDP_TIMEDATE64,
             4
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_BOOLEAN8,  /*	data type		*/
+            0               /*	no of elements	*/
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_CHAR8,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_UTF16,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_INT8,
+            0           
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_INT16,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_INT32,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_INT64,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_UINT8,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_UINT16,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_UINT32,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_UINT64,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_REAL32,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_REAL64,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_TIMEDATE32,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_TIMEDATE48,
+            0
+        },
+        {
+            TRDP_UINT16,
+            1
+        },
+        {
+            TRDP_TIMEDATE64,
+            0
+        },
+        {
+            993,
+            1
         }
     }
+};
+
+struct myDataSet990
+{
+    UINT8   level;
+    char    string[16];
+};
+
+struct myDataSet991
+{
+    UINT8   level;
+    struct myDataSet990 ds;
+};
+
+struct myDataSet992
+{
+    UINT8   level;
+    struct myDataSet991 ds;
+};
+
+struct myDataSet993
+{
+    UINT8   level;
+    struct myDataSet992 ds;
 };
 
 struct myDataSet1000
@@ -168,8 +392,8 @@ struct myDataSet1000
     TIMEDATE48  timedate48_1;
     TIMEDATE64  timedate64_1;
     UINT8   bool8_4[4];
-    char    string[16];
-    INT16   utf16_4[4];
+    char    char8_16[16];
+    INT16   utf16_4[16];
     INT8    int8_4[4];
     INT16   int16_4[4];
     INT32   int32_4[4];
@@ -183,40 +407,106 @@ struct myDataSet1000
     TIMEDATE32  timedate32_4[4];
     TIMEDATE48  timedate48_4[4];
     TIMEDATE64  timedate64_4[4];
-} gMyDataSet1000 =
+    UINT16  size_bool8;
+    UINT8   bool8_0[4];
+    UINT16  size_char8;
+    char    char8_0[16];
+    UINT16  size_utf16;
+    INT16   utf16_0[16];
+    UINT16  size_int8;
+    INT8    int8_0[4];
+    UINT16  size_int16;
+    INT16   int16_0[4];
+    UINT16  size_int32;
+    INT32   int32_0[4];
+    UINT16  size_int32;
+    INT64   int64_0[4];
+    UINT16  size_int64;
+    UINT8   uint8_0[4];
+    UINT16  size_uint8;
+    UINT16  uint16_0[4];
+    UINT16  size_uint32;
+    UINT32  uint32_0[4];
+    UINT16  size_uint64;
+    UINT64  uint64_0[4];
+    UINT16  size_float32;
+    float   float32_0[4];
+    UINT16  size_float64;
+    double  float64_0[4];
+    UINT16  size_timedate32;
+    TIMEDATE32  timedate32_0[4];
+    UINT16  size_timedate48;
+    TIMEDATE48  timedate48_0[4];
+    UINT16  size_timedate64;
+    TIMEDATE64  timedate64_0[4];
+    struct myDataSet993 ds;
+} gMyDataSet999 =
 {
-    1,
+    1,                               /* BOOL8 */
     'A',
-    0xaa55,
-    -1,
-    4096,
+    0x0030,
+    0x12,
+    0x1234,
     0x12345678,
-    0x1234567890123456,
-    255,
-    65535,
+    0x123456789ABCDEF0,
+    0x12,
+    0x1234,
     0x12345678,
-    0x1234567890123456,
+    0x123456789ABCDEF0,
     0.12345,
     0.12345678,
-    0x01234567,
-    {0x01234567, 0x89AB},
-    {0x01234567, 0x89ABCDEF},
-    {1,0,1,0},
-    "Hello World",
-    {0xaa51, 0xaa52, 0xaa53, 0}
-    {-1, -2, -3, -4}
-    {4091, 4092, 4093, 4094},  
-    {0x12345678, 0x12345678, 0x12345678, 0x12345678},
-    {0x1234567890123451, 0x1234567890123452, 0x1234567890123453, 0x1234567890123454},
-    {251, 252, 253, 254},
-    {65531, 65532, 65533, 65534},
+    0x12345678,
+    {0x12345678, 0x9ABC},
+    {0x12345678, 0x9ABCDEF0},
+    {1,0,1,0},                      /* BOOL8 array fixed size */
+    "Hello old World",
+    {0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037, 0x0038, 0x0039, 0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0},
+    {0x12, 0x34, 0x55, 0x78},
+    {0x1234, 0x5678, 0x9ABC, 0xDEF0}
     {0x12345671, 0x12345672, 0x12345673, 0x12345674},
-    {0x1234567890123456, 0x1234567890123456, 0x1234567890123456, 0x1234567890123456},
-    {0.12345, 12345, 12345, 12345},
-    {0.12345678, 0.12345678, 0.12345678, 0.12345678},
-    {0x01234567, 0x01234567,0x01234567,0x01234567},
-    {{0x01234567, 0x89AB},{0x01234567, 0x89AB},{0x01234567, 0x89AB},{0x01234567, 0x89AB}},
-    {{0x01234567, 0x89ABCDEF},{0x01234567, 0x89ABCDEF},{0x01234567, 0x89ABCDEF}}
+    {0x123456789ABCDEF1, 0x123456789ABCDEF2, 0x123456789ABCDEF3, 0x123456789ABCDEF4},
+    {0x01, 0x23, 0x45, 0x67},
+    {0x1234, 0x5678, 0x9ABC, 0xDEF0},
+    {0x12345671, 0x12345672, 0x12345673, 0x12345674},
+    {0x123456789ABCDEF1, 0x123456789ABCDEF2, 0x123456789ABCDEF3, 0x123456789ABCDEF4},
+    {0.12341, 0x12342, 0x12343, 0x12344},
+    {0.12345671, 0.12345672, 0.12345673, 0.12345674},
+    {0x12345671, 0x12345672, 0x12345673, 0x12345674},
+    {{0x12345671, 0x89A1},{0x12345672, 0x89A2},{0x12345673, 0x89A3},{0x12345674, 0x89A4}},
+    {{0x12345671, 0x89ABCDE1},{0x12345672, 0x89ABCDE2},{0x12345673, 0x89ABCDE3}, {0x12345674, 0x89ABCDE4}}
+    4,
+    {1,0,1,0},                       /* BOOL8 array var size */
+    16,
+    "Hello old World",
+    16,
+    {0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037, 0x0038, 0x0039, 0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0},
+    4,
+    {0x12, 0x34, 0x55, 0x78},
+    4,
+    {0x1234, 0x5678, 0x9ABC, 0xDEF0},
+    4,
+    {0x12345671, 0x12345672, 0x12345673, 0x12345674},
+    4,
+    {0x123456789ABCDEF1, 0x123456789ABCDEF2, 0x123456789ABCDEF3, 0x123456789ABCDEF4},
+    4,
+    {0x12, 0x34, 0x55, 0x78},
+    4,
+    {0x1234, 0x5678, 0x9ABC, 0xDEF0},
+    4,
+    {0x12345671, 0x12345672, 0x12345673, 0x12345674},
+    4,
+    {0x123456789ABCDEF1, 0x123456789ABCDEF2, 0x123456789ABCDEF3, 0x123456789ABCDEF4},
+    4,
+    {0.12341, 0.12342, 0.12343, 0x12344},
+    4,
+    {0.12345671, 0.12345672, 0.12345673, 0.12345674},
+    4,
+    {0x12345671, 0x12345672, 0x12345673, 0x12345674},
+    4,
+    {{0x12345671, 0x89A1},{0x12345672, 0x89A2},{0x12345673, 0x89A3},{0x12345674, 0x89A4}},
+    4,
+    {{0x12345671, 0x89ABCDE1},{0x12345672, 0x89ABCDE2},{0x12345673, 0x89ABCDE3}, {0x12345674, 0x89ABCDE4}},
+    {1, {2, {3, {4, "Nested Datasets"}}}}
 };
 
 UINT8 gDstDataBuffer[1500];
