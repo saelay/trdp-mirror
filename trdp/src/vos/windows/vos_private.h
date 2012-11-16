affb76c410948b1c9302c63cd8b2a75a8fdc6a77
97025 <97025@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2012-11-16 18:33:46 +0000

    Memory handling engaged (is now target independend)
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@142 3b5a3598-5f4e-4449-9e63-bd40438bfec0

---
+++ trdp/src/vos/windows/vos_private.h
@@ -0,0 +1,55 @@
+/**********************************************************************************************************************/
+/**
+ * @file            vos_private.h
+ *
+ * @brief           Private definitions for the OS abstraction layer
+ *
+ * @details			
+ *
+ * @note            Project: TCNOpen TRDP prototype stack
+ *
+ * @author          Bernd Loehr, NewTec GmbH
+ *
+ * @remarks All rights reserved. Reproduction, modification, use or disclosure
+ *          to third parties without express authority is forbidden,
+ *          Copyright Bombardier Transportation GmbH, Germany, 2012.
+ *
+ *
+ * $Id$
+ *
+ */
+
+#ifndef VOS_PRIVATE_H
+#define VOS_PRIVATE_H
+
+/***********************************************************************************************************************
+ * INCLUDES
+ */
+
+#include <pthread.h>
+
+#include "vos_types.h"
+#include "vos_thread.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+/***********************************************************************************************************************
+ * DEFINES
+ */
+    
+struct VOS_MUTEX
+{
+    UINT32          magicNo;
+    pthread_mutex_t mutexId;
+};
+
+VOS_ERR_T vos_localMutexCreate (struct VOS_MUTEX *pMutex);
+VOS_ERR_T vos_mutexLocalDelete (struct VOS_MUTEX *pMutex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VOS_UTILS_H */
