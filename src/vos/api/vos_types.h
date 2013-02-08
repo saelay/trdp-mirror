8a0dadfcbc78ccd6855a1393454fc4a089b4d9f0
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2013-02-08 10:33:36 +0000

    vos_initRuntimeConsts added
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk/trdp@488 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- src/vos/api/vos_types.h
+++ src/vos/api/vos_types.h
@@ -130,6 +130,7 @@ typedef enum
     VOS_MUTEX_ERR       = -12,  /**< Mutex not available                             */
     VOS_THREAD_ERR      = -13,  /**< Thread creation error                           */
     VOS_BLOCK_ERR       = -14,  /**< System call would have blocked in blocking mode */
+    VOS_INTEGRATION_ERR = -15,  /**< Alignment or endianess for selected target wrong */
     VOS_UNKNOWN_ERR     = -99   /**< Unknown error                                   */
 } VOS_ERR_T;
 
