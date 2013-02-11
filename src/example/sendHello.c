8afb34b12a3c537728a91c567bc21f015e90414c
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2013-02-11 10:38:19 +0000

    select() needs max. descriptor + 1
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk/trdp@509 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- src/example/sendHello.c
+++ src/example/sendHello.c
@@ -291,7 +291,7 @@ int main (int argc, char *argv[])
            Select() will wait for ready descriptors or time out,
            what ever comes first.
          */
-        rv = select((int)noDesc, &rfds, NULL, NULL, &tv);
+        rv = select((int)noDesc + 1, &rfds, NULL, NULL, &tv);
 
         /*
            Check for overdue PDs (sending and receiving)
