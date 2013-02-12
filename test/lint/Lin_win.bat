918c5080440a22e73008fbebdaaf6f26bc6671af
aweiss <aweiss@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2013-02-12 14:15:22 +0000

    MISRA rule checking added
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk/trdp@514 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- test/lint/Lin_win.bat
+++ test/lint/Lin_win.bat
@@ -1,4 +1,4 @@
-"c:\Lint\Lint-nt" +v -i"c:\Lint" std_win.lnt -DWIN32;MD_SUPPORT=1;__LITTLE_ENDIAN__=1;__BIG_ENDIAN__=0  -os(_LINT.TMP) trdp_if.c trdp_mdcom.c trdp_pdcom.c trdp_utils.c trdp_stats.c vos_utils.c vos_mem.c vos_thread.c vos_sock.c tau_marshall.c tau_xml.c
+"c:\Lint\Lint-nt" +v -i"c:\Lint" std_win.lnt -DWIN32;MD_SUPPORT=1;__LITTLE_ENDIAN__=1;__BIG_ENDIAN__=0;__VA_ARGS__ -os(_LINT.TMP) vos_utils.c vos_mem.c vos_thread.c vos_sock.c trdp_if.c trdp_mdcom.c trdp_pdcom.c trdp_utils.c trdp_stats.c tau_marshall.c tau_xml.c
 type _LINT.TMP | more
 @echo off
 echo ---
