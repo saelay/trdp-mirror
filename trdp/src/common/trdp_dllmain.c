41aa8db86ff2da56a9ad34ff961ece62cf10c6e4
aweiss <aweiss@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2013-05-15 15:01:09 +0000

    Description updated
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@817 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/src/common/trdp_dllmain.c
+++ trdp/src/common/trdp_dllmain.c
@@ -1,12 +1,42 @@
-#define WIN32_LEAN_AND_MEAN             // Selten verwendete Teile der Windows-Header nicht einbinden.
+/**********************************************************************************************************************/
+/**
+ * @file            trdp_dllmain.c
+ *
+ * @brief           Windows DLL main function
+ *
+ * @details
+ *
+ * @note            Project: TCNOpen TRDP prototype stack
+ *
+ * @author          Armin-H. Weiss, Bombardier
+ *
+ * @remarks All rights reserved. Reproduction, modification, use or disclosure
+ *          to third parties without express authority is forbidden,
+ *          Copyright Bombardier Transportation GmbH, Germany, 2012.
+ *
+ *
+ * $Id: trdp_if.c 814 2013-05-15 09:37:29Z 97025 $
+ *
+ */
+
+/***********************************************************************************************************************
+ * INCLUDES
+ */
+#ifdef WIN32
+#ifdef DLL_EXPORT
+
+#define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
 #include "trdp_if.h"
 #include "trdp_private.h"
 #include "vos_utils.h"
 
-#ifdef WIN32
-#ifdef DLL_EXPORT
+
+/***********************************************************************************************************************
+ * GLOBAL FUNCTIONS
+ */
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -15,24 +45,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
         case DLL_PROCESS_ATTACH:
-        {
-            const TRDP_VERSION_T *pVersion;
-
-            /* check lib version */
-            pVersion = tlc_getVersion();
-
-            if (   (pVersion->ver != TRDP_VERSION)
-                || (pVersion->rel <  TRDP_RELEASE)
-               )
-            {
-                vos_printf( VOS_LOG_ERROR, "ERROR: Wrong version of TRDP.DLL (expected: %d.%d.%d.%d - found: %d.%d.%d.%d)\n\n",
-                        TRDP_VERSION, TRDP_RELEASE, TRDP_UPDATE, TRDP_EVOLUTION,
-                        (int)pVersion->ver, (int)pVersion->rel, (int)pVersion->upd, (int)pVersion->evo );
-
-                return FALSE;
-            }
-        }
-        break;
         case DLL_THREAD_ATTACH:
         case DLL_THREAD_DETACH:
         case DLL_PROCESS_DETACH:
