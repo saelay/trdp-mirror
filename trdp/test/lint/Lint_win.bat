e4910f2391fd5ccce81d25e512aebcd8f5e4a087
aweiss <aweiss@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2013-05-17 08:48:15 +0000

    Lint file renamed
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@829 3b5a3598-5f4e-4449-9e63-bd40438bfec0

---
+++ trdp/test/lint/Lint_win.bat
@@ -0,0 +1,6 @@
+"c:\Lint\Lint-nt" +v -i"c:\Lint" std_win.lnt -DWIN32;MD_SUPPORT=1;__LITTLE_ENDIAN__=1;__BIG_ENDIAN__=0 -os(_LINT_WIN.TMP) vos_utils.c vos_mem.c vos_thread.c vos_sock.c trdp_if.c trdp_mdcom.c trdp_pdcom.c trdp_utils.c trdp_stats.c tau_marshall.c tau_xml.c
+type _LINT_WIN.TMP | more
+@echo off
+echo ---
+echo  output placed in _LINT_WIN.TMP
+echo --- 
