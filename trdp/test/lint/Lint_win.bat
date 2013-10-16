447869ef6cca4affcda453b75fe36a3614b211b2
ahweiss <ahweiss@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2013-10-16 09:05:05 +0000

    vos_shared_mem.c added
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@1106 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/test/lint/Lint_win.bat
+++ trdp/test/lint/Lint_win.bat
@@ -1,4 +1,4 @@
-"c:\Program Files\Lint\Lint-nt" +v -i"c:\Program Files\Lint\lnt" std_win.lnt -DWIN32;MD_SUPPORT=1;__LITTLE_ENDIAN__=1;__BIG_ENDIAN__=0 -os(_LINT_WIN.TMP) vos_utils.c vos_mem.c vos_thread.c vos_sock.c trdp_if.c trdp_mdcom.c trdp_pdcom.c trdp_utils.c trdp_stats.c tau_marshall.c tau_xml.c
+"c:\Program Files\Lint\Lint-nt" +v -i"c:\Program Files\Lint\lnt" std_win.lnt -DWIN32;UNICODE;MD_SUPPORT=1;__LITTLE_ENDIAN__=1;__BIG_ENDIAN__=0 -os(_LINT_WIN.TMP) vos_utils.c vos_mem.c vos_thread.c vos_sock.c vos_shared_mem.c trdp_if.c trdp_mdcom.c trdp_pdcom.c trdp_utils.c trdp_stats.c tau_marshall.c tau_xml.c
 type _LINT_WIN.TMP | more
 @echo off
 echo ---
