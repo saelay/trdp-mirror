9cd4a677b14a8a05b2af56f5af9ab4b9c7249d86
railroad-mike <railroad-mike@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2014-07-09 09:30:56 +0000

    moved the short version structure to the communication definitions and added the packed attribute. problem arose using the ARM OTN compiler
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@1256 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/src/api/trdp_types.h
+++ trdp/src/api/trdp_types.h
@@ -89,12 +89,6 @@ typedef struct
     UINT8   evo;    /**< Evolution  - incremented for build                */
 } TRDP_VERSION_T;
 
-typedef struct
-{
-    UINT8   ver;    /**< Version    - incremented for incompatible changes */
-    UINT8   rel;    /**< Release    - incremented for compatible changes   */
-} TRDP_SHORT_VERSION_T;
-
 /** Return codes for all API functions, -1..-29 taken over from vos */
 typedef enum
 {
