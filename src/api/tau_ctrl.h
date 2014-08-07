a3a7cb71118a9c208f2d9fe90df9ff17cdeb5229
ahweiss <ahweiss@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2014-08-07 05:59:55 +0000

    1st version of tau_ctrl.c
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk/trdp@1277 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- src/api/tau_ctrl.h
+++ src/api/tau_ctrl.h
@@ -44,6 +44,11 @@ extern "C" {
 #define __cdecl
 #endif
 
+#define ECSP_CTRL_CYCLE         1000000             /* in us (1000000 = 1 sec) acc. 61375-2-3                  */
+#define ECSP_CTRL_TIMEOUT       5000000             /* in us (5000000 = 5 sec) acc. 61375-2-3                  */
+#define ECSP_STAT_TIMEOUT       5000000             /* in us (5000000 = 5 sec) acc. 61375-2-3                  */
+#define ECSP_CONF_REPLY_TIMEOUT 3000000             /* in us (3000000 = 3 sec) acc. 61375-2-3                  */
+
 
 /***********************************************************************************************************************
  * TYPEDEFS
@@ -62,65 +67,80 @@ extern "C" {
 /**********************************************************************************************************************/
 /**    Function to init  ECSP control interface
  *
- *  @param[in]      TRDP_APP_SESSION_T  appHndl
+ *  @param[in]      appHandle           Application handle
+ *  @param[in]      ecspIpAddr          ECSP address
+ *  @param[in]      ecscIpAddr          ECSC address
  *
  *  @retval         TRDP_NO_ERR         no error
  *  @retval         TRDP_INIT_ERR       initialisation error
  *
  */
-EXT_DECL TRDP_ERR_T tau_initEcspCtrl ( TRDP_APP_SESSION_T appHndl );
+EXT_DECL TRDP_ERR_T tau_initEcspCtrl ( TRDP_APP_SESSION_T   appHandle, 
+                                       TRDP_IP_ADDR_T       ecspIpAddr, 
+                                       TRDP_IP_ADDR_T       ecscIpAddr);
 
 
 
 /**********************************************************************************************************************/
 /**    Function to close  ECSP control interface
  *
+ *  @param[in]      appHandle           Application handle
+ *
  *  @retval         TRDP_NO_ERR         no error
  *  @retval         TRDP_UNKNOWN_ERR    undefined error
  *
  */
 
-EXT_DECL TRDP_ERR_T tau_terminateEcspCtrl (void);
+EXT_DECL TRDP_ERR_T tau_terminateEcspCtrl ( TRDP_APP_SESSION_T   appHandle );
 
 
 /**********************************************************************************************************************/
 /**    Function to set ECSP control information
  *
- *
+ *  @param[in]      appHandle       Application handle
  *  @param[in]      pEcspCtrl       Pointer to the ECSP control structure
  *
  *  @retval         TRDP_NO_ERR     no error
+ *  @retval         TRDP_NOINIT_ERR module not initialised
  *  @retval         TRDP_PARAM_ERR  Parameter error
  *
  */
-EXT_DECL TRDP_ERR_T tau_setEcspCtrl (TRDP_ECSP_CTRL_T  *pEcspCtrl);
+EXT_DECL TRDP_ERR_T tau_setEcspCtrl ( TRDP_APP_SESSION_T   appHandle,
+                                      TRDP_ECSP_CTRL_T  *pEcspCtrl);
 
 
 /**********************************************************************************************************************/
 /**    Function to get ECSP status information
  *
- *
- *  @param[in/out]   pEcspStat       Pointer to the ECSP status structure
+ *  @param[in]      appHandle       Application Handle
+ *  @param[in/out]  pEcspStat       Pointer to the ECSP status structure
+ *  @param[in/out]  pPdInfo         Pointer to PD status information
  *
  *  @retval         TRDP_NO_ERR     no error
+ *  @retval         TRDP_NOINIT_ERR module not initialised
  *  @retval         TRDP_PARAM_ERR  Parameter error
  *
  */
-EXT_DECL TRDP_ERR_T tau_getEcspStat (TRDP_ECSP_STAT_T  *pEcspStat);
+EXT_DECL TRDP_ERR_T tau_getEcspStat ( TRDP_APP_SESSION_T   appHandle,
+                                      TRDP_ECSP_STAT_T    *pEcspStat,
+                                      TRDP_PD_INFO_T      *pPdInfo);
 
 
 /**********************************************************************************************************************/
-/**    Function for ECSP confirmation/correction request
+/**    Function for ECSP confirmation/correction request, reply will be received via call back
  *
- *
- *  @param[in]      pEcspCtrl       Pointer to the ECSP control structure
+ *  @param[in]      TRDP_APP_SESSION_T  appHandle
+ *  @param[in]      pUserRef            user reference returned with reply
+ *  @param[in]      EcspConfRequest     Pointer to confirmation data       
  *
  *  @retval         TRDP_NO_ERR     no error
+ *  @retval         TRDP_NOINIT_ERR module not initialised
  *  @retval         TRDP_PARAM_ERR  Parameter error
  *
  */
-EXT_DECL TRDP_ERR_T tau_requestEcspConfirm (TRDP_ECSP_CONF_REQUEST_T  *pEcspConfRequest, 
-                                            TRDP_ECSP_CONF_REPLY_T    *pEcspConfReply);
+EXT_DECL TRDP_ERR_T tau_requestEcspConfirm ( TRDP_APP_SESSION_T         appHandle,
+                                             const void                *pUserRef, 
+                                             TRDP_ECSP_CONF_REQUEST_T  *pEcspConfRequest);
 
 
 #ifdef __cplusplus
