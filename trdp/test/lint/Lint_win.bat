d3f8996de4b110374d18e72c0bf09ff51aa1147a
ahweiss <ahweiss@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2014-08-27 12:05:25 +0000

    tau_ctrl.c tau_dnr.c tau_tti.c added
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@1315 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/test/lint/Lint_win.bat
+++ trdp/test/lint/Lint_win.bat
@@ -1,4 +1,4 @@
-"c:\Program Files\Lint\Lint-nt" +v -i"c:\Program Files\Lint\lnt" std_win.lnt -DWIN32;UNICODE;MD_SUPPORT=1;L_ENDIAN;__LITTLE_ENDIAN__=1;__BIG_ENDIAN__=0 -os(_LINT_WIN.TMP) vos_utils.c vos_mem.c vos_thread.c vos_sock.c vos_shared_mem.c trdp_if.c trdp_mdcom.c trdp_pdcom.c trdp_utils.c trdp_stats.c tau_marshall.c tau_xml.c
+"c:\Program Files\Lint\Lint-nt" +v -i"c:\Program Files\Lint\lnt" std_win.lnt -DWIN32;UNICODE;MD_SUPPORT=1;L_ENDIAN;__LITTLE_ENDIAN__=1;__BIG_ENDIAN__=0 -os(_LINT_WIN.TMP) vos_utils.c vos_mem.c vos_thread.c vos_sock.c vos_shared_mem.c trdp_if.c trdp_mdcom.c trdp_pdcom.c trdp_utils.c trdp_stats.c tau_marshall.c tau_xml.c tau_ctrl.c tau_dnr.c tau_tti.c
 type _LINT_WIN.TMP | more
 @echo off
 echo ---
