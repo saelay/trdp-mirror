5ff2c7891d38ebbb798595a3be789807c629a0e5
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2014-08-27 15:05:54 +0000

    Ticket 62: Data size error on MD-TCP fixed (crash fixed, MD-test still fails)
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@1316 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/src/api/trdp_proto.h
+++ trdp/src/api/trdp_proto.h
@@ -59,9 +59,9 @@
 
 /*  PD packet properties    */
 #define TRDP_MIN_PD_HEADER_SIZE     sizeof(PD_HEADER_T)                 /**< PD header size with FCS                */
-#define TRDP_MAX_PD_DATA_SIZE       1432                                /**< PD data size without FCS               */
-#define TRDP_MAX_PD_PACKET_SIZE     (TRDP_MAX_PD_DATA_SIZE + TRDP_MIN_PD_HEADER_SIZE + sizeof(UINT32))
-#define TRDP_MAX_MD_DATA_SIZE       ((1024 * 64) - 8 - sizeof(MD_HEADER_T))
+#define TRDP_MAX_PD_DATA_SIZE       1432                                /**< PD data                                */
+#define TRDP_MAX_PD_PACKET_SIZE     (TRDP_MAX_PD_DATA_SIZE + TRDP_MIN_PD_HEADER_SIZE)
+#define TRDP_MAX_MD_DATA_SIZE       (65388)
 #define TRDP_MAX_MD_PACKET_SIZE     (TRDP_MAX_MD_DATA_SIZE + sizeof(MD_HEADER_T))
 
 /** Maximum values    */
