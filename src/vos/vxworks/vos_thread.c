a9a0556cdbeec2cc864755bc5aee176e0d9de457
railroad-mike <railroad-mike@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2014-09-23 09:27:40 +0000

    did some finetuning and renaming on the cyclic thred helper function, though copied it to each platform directory - this needs some re-consideration and tuning in the includes, should satisfy #35
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk/trdp@1334 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- src/vos/vxworks/vos_thread.c
+++ src/vos/vxworks/vos_thread.c
@@ -55,37 +55,85 @@ INT32           vosThreadInitialised = FALSE;
 /***********************************************************************************************************************
  *  LOCALS
  */
-/**********************************************************************************************************************/
-/** Cyclic thread functions.
- *  Wrapper for cyclic threads. The thread function will be called cyclically with interval.
- *
- *  @param[in]      interval        Interval for cyclic threads in us (optional)
- *  @param[in]      pFunction       Pointer to the thread function
- *  @param[in]      pArguments      Pointer to the thread function parameters
- *  @retval         void
- */
-
-void cyclicThread (
-    UINT32              interval,
-    VOS_THREAD_FUNC_T   pFunction,
-    void                *pArguments)
-{
-    for (;; )
-    {
-        (void) vos_threadDelay(interval);
-        pFunction(pArguments);
-        /*pthread_testcancel(); deactivated - no POSIX any longer*/
-    }
-}
 
 /***********************************************************************************************************************
  * GLOBAL FUNCTIONS
  */
 
-
 /**********************************************************************************************************************/
 /* Threads 
  */
+
+/**********************************************************************************************************************/
+/** Cyclic thread functions.
+ *  Wrapper for cyclic threads. The thread function will be called cyclically with interval.
+ *
+ *  @param[in]      interval        Interval for cyclic threads in us (incl. runtime)
+ *  @param[in]      pFunction       Pointer to the thread function
+ *  @param[in]      pArguments      Pointer to the thread function parameters
+ *  @retval         void
+ */
+
+#define NSECS_PER_USEC  1000
+#define USECS_PER_MSEC  1000
+#define MSECS_PER_SEC   1000
+
+/* This define holds the max amount os seconds to get stored in 32bit holding micro seconds        */
+/* It is the result when using the common time struct with tv_sec and tv_usec as on a 32 bit value */
+/* so far 0..999999 gets used for the tv_usec field as per definition, then 0xFFF0BDC0 usec        */ 
+/* are remaining to represent the seconds, which in turn give 0x10C5 seconds or in decimal 4293    */ 
+#define MAXSEC_FOR_USECPRESENTATION 4293U
+
+void vos_cyclicThread (
+    UINT32              interval,
+    VOS_THREAD_FUNC_T   pFunction,
+    void                *pArguments)
+{
+    VOS_TIME_T priorCall;
+    VOS_TIME_T afterCall;
+    UINT32 execTime;
+    UINT32 waitingTime;
+    for (;; )
+    {
+        vos_getTime(&priorCall);  /* get initial time */
+        pFunction(pArguments);    /* perform thread function */
+        vos_getTime(&afterCall);  /* get time after function ghas returned */
+        /* subtract in the pattern after - prior to get the runtime of function() */
+        vos_subTime(&afterCall,&priorCall);
+        /* afterCall holds now the time difference within a structure not compatible with interval */
+        /* check if UINT32 fits to hold the waiting time value */
+        if (afterCall.tv_sec <= MAXSEC_FOR_USECPRESENTATION)
+        {
+            /*           sec to usec conversion                               value normalized from 0 .. 999999*/
+            execTime = ((afterCall.tv_sec * MSECS_PER_SEC * USECS_PER_MSEC) + (UINT32)afterCall.tv_usec);
+            if (execTime > interval)
+            {
+                /*severe error: cyclic task time violated*/
+                waitingTime = 0U;
+                /* Log the runtime violation */
+                vos_printLog(VOS_LOG_ERROR,
+                             "cyclic thread with interval %d usec was running  %d usec\n",
+                             interval, execTime);
+            }
+            else
+            {
+                waitingTime = interval - execTime;
+            }
+        } 
+        else
+        {
+            /* seems a very critical overflow has happened - or simply a misconfiguration */
+            /* as a rough first guess use zero waiting time here */
+            waitingTime = 0U;
+            /* Have this value range violation logged */
+            vos_printLog(VOS_LOG_ERROR,
+                         "cyclic thread with interval %d usec exceeded time out by running %d sec\n",
+                         interval, afterCall.tv_sec);
+        }
+        (void) vos_threadDelay(waitingTime);
+        pthread_testcancel();
+    }
+}
 
 /**********************************************************************************************************************/
 /** Initialize the thread library.
