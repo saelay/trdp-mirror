4f0d59e0689201c47294cf0a0d75f75a678f7500
newtecbosse <newtecbosse@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2016-03-11 12:16:42 +0000

    topocount crc check
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@1554 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/src/common/tau_tti.c
+++ trdp/src/common/tau_tti.c
@@ -170,7 +170,7 @@ static void ttiPDCallback (
 
             /* check the crc:   */
             crc = vos_sc32(0xFFFFFFFF, (const UINT8 *) &pTelegram->state, sizeof(TRDP_OP_TRAIN_DIR_STATE_T) - 4);
-            if (crc != MAKE_LE(pTelegram->state.crc))
+            if (crc != vos_ntohl(pTelegram->state.crc))
             {
                 vos_printLog(VOS_LOG_ERROR, "CRC error of received operational status info (%08x != %08x)!\n",
                              crc, vos_ntohl(pTelegram->state.crc))
