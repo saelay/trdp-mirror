e21830ff46dc24d406e997e47b736c634a6eab22
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2016-03-22 14:53:38 +0000

    Ticket #116 fixed in 1.3.0.1
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/tags/trdp/1.3.0.1@1558 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- src/common/trdp_private.h
+++ src/common/trdp_private.h
@@ -44,7 +44,7 @@
 #define TRDP_VERSION            1
 #define TRDP_RELEASE            3
 #define TRDP_UPDATE             0
-#define TRDP_EVOLUTION          0
+#define TRDP_EVOLUTION          1
 #endif
 
 #define TRDP_TIMER_GRANULARITY  10000                                   /**< granularity in us                      */
