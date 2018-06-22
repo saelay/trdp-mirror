3395acffc2cc92de998719fd0eeb4031c3363892
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2018-06-22 16:53:53 +0000

    propset
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk/trdp@1742 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- component.mk
+++ component.mk
@@ -16,7 +16,7 @@
 #*          If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #*          Copyright 2018. All rights reserved.
 #*
-#* $Id: $
+#* $Id$
 #
 #
 
