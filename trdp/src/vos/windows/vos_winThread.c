3e580b646a9b0b00be62515e91d2a790d852eada
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2018-07-20 17:10:52 +0000

    vos_threadSelf added
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@1752 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/src/vos/windows/vos_winThread.c
+++ trdp/src/vos/windows/vos_winThread.c
@@ -316,6 +316,27 @@ EXT_DECL VOS_ERR_T vos_threadIsActive (
 }
 
 /**********************************************************************************************************************/
+/** Return thread handle of calling task
+ *
+ *  @param[out]     pThread         pointer to thread handle
+ *  @retval         VOS_NO_ERR      no error
+ *  @retval         VOS_PARAM_ERR   parameter out of range/invalid
+ */
+
+EXT_DECL VOS_ERR_T vos_threadSelf (
+    VOS_THREAD_T *pThread)
+{
+    if (pThread == NULL)
+    {
+        return VOS_PARAM_ERR;
+    }
+
+    *pThread = (VOS_THREAD_T) GetCurrentThread();
+
+    return VOS_NO_ERR;
+}
+
+/**********************************************************************************************************************/
 /*  Timers                                                                                                            */
 /**********************************************************************************************************************/
 
